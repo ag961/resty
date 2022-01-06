78afac3f574e80e5872b59e3723deed84ce9857f
Ayrat <gimranov45@gmail.com>
2022-01-06 11:02:16 +0300

    finished lab29, fixed deployment

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef, useReducer } from 'react';
+import React, { useEffect, useRef, useReducer } from 'react';
 import axios from 'axios';
 
 import './app.scss';
